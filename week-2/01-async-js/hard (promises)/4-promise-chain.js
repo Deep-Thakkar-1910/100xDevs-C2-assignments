2a15ea1d673aa57cf72ddbac7678c5dfda73b6f3
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2023-12-11 19:26:20 +0530

    Assignment Submission

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -24,11 +24,25 @@ function wait3(t) {
         setTimeout(()=>resolve(t*1000),t*1000); // resolve the time given to them 
     });
 }
+
 function calculateTime(t1, t2, t3) {
     return wait1(t1)
     .then((time1)=>wait2(t2).then((time2)=>time1+time2))
-    .then((time12)=>wait3(t3).then((time3)=>time12+time3));
-
+    .then((time12)=>wait3(t3).then((time3)=>time12+time3)); // this step is very untidy and scales our code horizontally and should be avoided however i did it because it was mentioned in the instructions.    
 }
+
+/* 
+// The better way to do it is using async await : 
+
+async function calculateTime(t1, t2, t3) {
+    let totalTime = 0;
+    const time1 =  await wait1(t1)
+    const time2 =  await wait2(t2)
+    const time3 =  await wait3(t3)
+    return time1+time2+time3;
+} */
+
+
+
 
 module.exports = calculateTime;
